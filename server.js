9ff310770baa3d6d998410a18fef9bdcddf6d9d1
leolm83 <leonardo.mateus01@hotmail.com>
2023-11-05 10:14:35 -0300

    rotas basicas

--- server.js
+++ server.js
@@ -3,10 +3,23 @@ import { fastify} from 'fastify'
 const server = fastify();
 
 server.get('/', ()=>{
-    return 'HELLO WORLD!!'
+    return 'ACESSE /videos para obter um retorno desejado'
 })
 
-server.get('/rocket', ()=>{
+server.get('/videos', ()=>{
+    return 'HELLO ROCKET!!'
+})
+
+
+server.get('/videos/:id', ()=>{
+    return 'HELLO ROCKET!!'
+})
+
+server.put('/videos/:id', ()=>{
+    return 'HELLO ROCKET!!'
+})
+
+server.delete('/videos/:id', ()=>{
     return 'HELLO ROCKET!!'
 })
 
